42f94ffdfaff93371f7d01afaa4207c24525143f
Ana Rebollo Pin <ana.rebollo.pin@est.fib.upc.edu>
2013-01-09 15:26:48 +0100

    -

---
+++ src/procuidado/cuidadores/EsElUltimoCuidadorDelResidente.java
@@ -0,0 +1,8 @@
+package procuidado.cuidadores;
+
+
+public class EsElUltimoCuidadorDelResidente extends Exception{	
+	public EsElUltimoCuidadorDelResidente(String error) {
+		System.out.println(error);
+	}
+}
